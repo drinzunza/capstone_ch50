8f136149b16a8fba1f868b03c953631c75fa97e5
Sergio Inzunza <inxunxa@gmail.com>
2025-01-02 20:37:33 -0700

    class 5

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserLoginView, user_logout
+from .views import UserLoginView, user_logout, SignUpView
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', user_logout, name='logout'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy, reverse
 from django.shortcuts import redirect
 from django.contrib.auth import logout
-
+from django.views.generic.edit import CreateView
+from .forms import SignupForm
+from .models import Profile
 
 """
 Class-based views:
@@ -26,6 +28,24 @@ class UserLoginView(LoginView):
     def get_success_url(self):
         return reverse('home')
 
+
+class SignUpView(CreateView):
+    template_name = "users/signup.html"
+    form_class = SignupForm
+    success_url = reverse_lazy('login')
+
+    # extend the save functionality
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        passw = form.cleaned_data['password']
+        user.set_password(passw) # hash/encrypt the password
+        user.save()
+
+        # create an empty profile for the user
+        Profile.objects.create(user=user)
+
+        return super().form_valid(form)
+    
 
 def user_logout(request):
     logout(request)
